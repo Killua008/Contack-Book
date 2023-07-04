a3ce3b596e4bbc9cff30215cc673fb467d7a2672
Sharad Billava <74694247+Killua008@users.noreply.github.com>
2023-07-04 09:47:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from tkinter import *
 import tkinter.messagebox as mb
 import sqlite3
 
-# Connecting and Initializing the Database where we will store all the data
+# Connecting and Initializing the Database for storing the data
 connector = sqlite3.connect('contacts.db')
 cursor = connector.cursor()
 
 cursor.execute(
 "CREATE TABLE IF NOT EXISTS CONTACT_BOOK (S_NO INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NAME TEXT, EMAIL TEXT, PHONE_NUMBER TEXT, ADDRESS TEXT)"
 )
+list1=[1,2,3,4,5,6,7,8,9,0]
 
-# Defining the functions
+
+# submiting a record
 def submit_record():
     global name_strvar, email_strvar, phone_strvar, address_entry
     global cursor
@@ -18,7 +20,22 @@ def submit_record():
 
     if name=='' or email=='' or phone=='' or address=='':
         mb.showerror('Error!', "Please fill all the fields!")
+    elif '@' not in email:
+        mb.showerror('Invalid Email',"Please input valid email")
+    elif len(phone)<10 :
+        mb.showerror('Invalid Phone no',"Please input valid Phone no.")
+    elif len(phone)>10:
+        c=0
+        for i in phone:
+            if i not in list1:
+                c+=1
+        if c>1:
+            mb.showerror('Invalid Phone no',"Please input valid Phone no.")
+    
     else:
+        #curr=cursor.execute("SELECT * FROM CONTACT_BOOK WHERE (?,?,?,?)", (name, email, phone, address))
+        #print(curr)
+        #print(curr)
         cursor.execute(
         "INSERT INTO CONTACT_BOOK (NAME, EMAIL, PHONE_NUMBER, ADDRESS) VALUES (?,?,?,?)", (name, email, phone, address))
         connector.commit()
@@ -26,7 +43,7 @@ def submit_record():
         listbox.delete(0, END)
         list_contacts()
         clear_fields()
-
+ 
 
 def list_contacts():
     curr = connector.execute('SELECT NAME FROM CONTACT_BOOK')
@@ -62,15 +79,37 @@ def delete_all_records():
 
 def view_record():
     global name_strvar, phone_strvar, email_strvar, address_entry, listbox
+    #curr = cursor.execute('SELECT * FROM CONTACT_BOOK WHERE NAME=?', listbox.get(ACTIVE))
+    try:
+        selected_name = listbox.get(listbox.curselection()) 
+        print(selected_name)
 
-    curr = cursor.execute('SELECT * FROM CONTACT_BOOK WHERE NAME=?', listbox.get(ACTIVE))
-    values = curr.fetchall()[0]
+    
+        cursor.execute('SELECT * FROM CONTACT_BOOK WHERE NAME=?', [selected_name])
+        values = cursor.fetchall()
 
-    name_strvar.set(values[1]); phone_strvar.set(values[3]); email_strvar.set(values[2])
+    
+    #values = curr.fetchall()[0]
+        print(values)
 
-    address_entry.delete(1.0, END)
-    address_entry.insert(END, values[4])
+        name_strvar.set(values[0][1]); phone_strvar.set(values[0][3]); email_strvar.set(values[0][2])
 
+        address_entry.delete(1.0, END)
+        address_entry.insert(END, values[0][4])
+    except:
+        #mb.showerror("Error","Unidentified Value")
+    
+        try:
+            curr = cursor.execute('SELECT * FROM CONTACT_BOOK WHERE NAME=?', listbox.get(ACTIVE))
+            values = curr.fetchall()[0]
+
+            name_strvar.set(values[1]); phone_strvar.set(values[3]); email_strvar.set(values[2])
+
+            address_entry.delete(1.0, END)
+            address_entry.insert(END, values[4])
+        except:
+            mb.showerror("Error","Unidentified Value")
+    
 
 def clear_fields():
     global name_strvar, phone_strvar, email_strvar, address_entry, listbox
@@ -98,24 +137,24 @@ def search():
 
 # Initializing the GUI window
 root = Tk()
-root.title("TechVidvan Contact Book")
-root.geometry('700x550')
+root.title("Contact Book")
+root.geometry('900x550')
 root.resizable(0, 0)
 
-# Creating the color and font variables
-lf_bg = 'Gray70'  # Lightest Shade
+# Color
+lf_bg = 'Gray70' 
 cf_bg = 'Gray57'
-rf_bg = 'Gray35'  # Darkest Shade
+rf_bg = 'Gray35'  
 frame_font = ("Garamond", 14)
 
-# Creating the StringVar variables
+#StringVar variables
 name_strvar = StringVar()
 phone_strvar = StringVar()
 email_strvar = StringVar()
 search_strvar = StringVar()
 
-# Creating and placing the components in the window
-Label(root, text='CONTACT BOOK', font=("Noto Sans CJK TC", 15, "bold"), bg='Black', fg='White').pack(side=TOP, fill=X)
+# Components
+Label(root, text='CONTACT BOOK', font=("Noto Sans CJK TC", 15, "bold"), bg='Black', fg='Grey').pack(side=TOP, fill=X)
 
 left_frame = Frame(root, bg=lf_bg)
 left_frame.place(relx=0, relheight=1, y=30, relwidth=0.3)
@@ -126,7 +165,7 @@ center_frame.place(relx=0.3, relheight=1, y=30, relwidth=0.3)
 right_frame = Frame(root, bg=rf_bg)
 right_frame.place(relx=0.6, relwidth=0.4, relheight=1, y=30)
 
-# Placing components in the left frame
+# Left frame
 Label(left_frame, text='Name', bg=lf_bg, font=frame_font).place(relx=0.3, rely=0.05)
 
 name_entry = Entry(left_frame, width=15, font=("Verdana", 11), textvariable=name_strvar)
@@ -147,7 +186,7 @@ Label(left_frame, text='Address', bg=lf_bg, font=frame_font).place(relx=0.28, re
 address_entry = Text(left_frame, width=15, font=("Verdana", 11), height=5)
 address_entry.place(relx=0.1, rely=0.55)
 
-# Placing components in the Middle Frame
+# Middle Frame
 search_entry = Entry(center_frame, width=18, font=("Verdana", 12), textvariable=search_strvar).place(relx=0.08, rely=0.04)
 
 Button(center_frame, text='Search', font=frame_font, width=15, command=search).place(relx=0.13, rely=0.1)
@@ -157,7 +196,7 @@ Button(center_frame, text='Clear Fields', font=frame_font, width=15, command=cle
 Button(center_frame, text='Delete Record', font=frame_font, width=15, command=delete_record).place(relx=0.13, rely=0.5)
 Button(center_frame, text='Delete All Records', font=frame_font, width=15, command=delete_all_records).place(relx=0.13, rely=0.6)
 
-# Placing components in the Right Frame
+# Right Frame
 Label(right_frame, text='Saved Contacts', font=("Noto Sans CJK TC", 14), bg=rf_bg).place(relx=0.25, rely=0.05)
 
 listbox = Listbox(right_frame, selectbackground='SkyBlue', bg='Gainsboro', font=('Helvetica', 12), height=20, width=25)
